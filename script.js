303bb276bf19c9fe4ceff632e9d1e8c80e0b3dd8
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-05 19:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function urlPage(){
+	let currentUrl = window.location.href;
+	let n = currentUrl.length;
+	alert("The length of the URL is:" +" "  + n);
+}
+urlPage();
